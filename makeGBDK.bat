23fa3f89bebb8961dae99008ed5fdaede3eb1ff1
Zalo <cebolleto@gmail.com>
2019-01-08 07:55:23 +0100

    make for gbdk

---
+++ makeGBDK.bat
@@ -0,0 +1,3 @@
+REM Automatically generated from Makefile
+%ZGB_PATH%\..\env\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o sound.o sound.c
+%ZGB_PATH%\..\env\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o sound.gb sound.o
